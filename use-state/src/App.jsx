c6d81ef52ee87559fd4cabfabcdbba642d2ce006
Hansfy <zoel8794@gmail.com>
2025-08-23 16:28:00 +0700

    Jhon Smith

--- use-state/src/App.jsx
+++ use-state/src/App.jsx
@@ -11,12 +11,34 @@ function App() {
   const decrement = () => {
     setCount(count - 1);
   };
-
+  const [friends, setFriends] = useState(["John", "Jane", "Doe"]);
+  const addFriend = () => {
+    setFriends([...friends, `Ini Teman Baru ${friends.length + 1}`]);
+  };
+  const removeFriend = () => {
+    setFriends(friends.slice(0, -1));
+  };
+  const updateFriend = () => {
+    setFriends(
+      friends.map((friend) => (friend === "John" ? "Jhon Smith" : friend))
+    );
+  };
   return (
     <section>
       <h1>{count}</h1>
       <button onClick={increment}>Tambah</button>
       <button onClick={decrement}>Kurang</button>
+      <h2>Daftar Teman</h2>
+      <div>
+        {friends.map((friend, index) => (
+          <p key={index}>
+            {index + 1}. {friend}
+          </p>
+        ))}
+        <button onClick={addFriend}>Tambah Teman</button>
+        <button onClick={removeFriend}>Hapus Teman</button>
+        <button onClick={updateFriend}>Update John</button>
+      </div>
     </section>
   );
 }
